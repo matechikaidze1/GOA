4b7c2a35803fe493a3a42fee1678ae6b34f59cdb
matechikaidze1 <tacocat78007@gmail.com>
2024-06-21 15:06:59 +0400

    commit

---
+++ day 16/homework/homework.py
@@ -0,0 +1,17 @@
+family_list = ['მათე', 'ზურა', 'მადონა']
+
+numbers2 = []
+for i in range(1, 11):
+    numbers.append(i)
+
+numbers2 = list(range(10, 21))
+
+
+first_name = input("Landon")
+last_name = input("Ricketts")
+age = int(input("50"))
+place_of_residence = input("New Austin")
+email = input("lanricks@gmail.com")
+
+customer_info = [first_name, last_name, age, place_of_residence, email]
+print(customer_info)
