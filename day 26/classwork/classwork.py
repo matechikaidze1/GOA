c4560ec7470877684a3d4709e8649bdc3f68a799
matechikaidze1 <tacocat78007@gmail.com>
2024-07-09 17:44:16 +0400

    commit

---
+++ day 26/classwork/classwork.py
@@ -0,0 +1,10 @@
+name = ("luka")
+capitalized_name = name.capitalize()
+
+print(capitalized_name)
+
+name2 = ("nika")
+name2 = name2.upper()
+print(name2)
+
+# capitalize ადიდებს მხოლოდ ტექსტის პირველ ასოს, ხოლო upper ადიდებს ყველა ასოს მოცემულ ტექსტში.
