040ccdf8c86f43de364eaa4e98500b413753c375
matechikaidze1 <tacocat78007@gmail.com>
2024-09-23 19:22:22 +0400

    commit

---
+++ day 58/classwork/task1.js
@@ -0,0 +1,15 @@
+const myStupidCat = {
+    name: "კოსტა",
+    age: 1,
+    color: "orange"
+
+};
+
+fav.name = "Fish"; //  კუთვნილებების შეცვლა
+fav.age = 2;
+
+fav.color = "Pink" 
+delete fav.color  // სავშალე color 
+console.log(fav)
+
+// object არის ნებისმიერი რამ რასაც ჩვენს გარშემო ვხედავთ
---
+++ day 58/homework/task1.js
@@ -0,0 +1,9 @@
+const car = {
+    brand: 'Toyota',
+    model: 'Camry',
+    year: 2021
+};
+ 
+console.log(car.brand);
+console.log(car.model);
+console.log(car.year);
