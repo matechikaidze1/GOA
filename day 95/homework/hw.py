a88d541da8966ea5c179dbb8b2b399587c782b0e
matechikaidze1 <tacocat78007@gmail.com>
2024-12-21 11:29:01 +0400

    commit

--- day 95/homework/hw.py
+++ day 95/homework/hw.py
@@ -1,1 +1,20 @@
-# in progress
+def func(arr):
+    if not arr:
+        return []
+    
+    arr1 = []
+    arr2 = [arr[0]]
+
+    for i in range(1, len(arr)):
+        if arr[i] > arr[i - 1]:
+            arr2.append(arr[i])
+        else:
+            if len(arr2) > len(arr1):
+                arr1 = arr2
+            arr2 = [arr[i]]
+
+        if len(arr2) > len(arr1):
+            arr1 = arr2
+
+        return arr1
+    print(func([1,3,1,4,5,6,8]))
